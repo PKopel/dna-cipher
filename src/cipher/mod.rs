f842e0a443248081906e17524277c7552b77427f
Paweł Kopel <pawel.kopel2@gmail.com>
2023-03-05 17:05:47 +0100

    Optimize copying between arrays

--- src/cipher/mod.rs
+++ src/cipher/mod.rs
@@ -16,15 +16,10 @@ const KEY_SIZE: usize = 8;
 const INTRON_SIZE: usize = 6;
 // intron size of 6 with target size of 18 uses 3 out of 4 pairs in key
 
-fn round(
-    input: &[DNA; INPUT_SIZE],
-    key: &[DNA; KEY_SIZE],
-    sbox: SBox,
-) -> ([DNA; SOURCE_SIZE], [DNA; TARGET_SIZE]) {
-    let mut source = [DNA::A; SOURCE_SIZE];
-    let mut target = [DNA::A; TARGET_SIZE];
-    source.copy_from_slice(&input[0..SOURCE_SIZE]);
-    target.copy_from_slice(&input[SOURCE_SIZE..INPUT_SIZE]);
+fn round(input: &[DNA; INPUT_SIZE], key: &[DNA; KEY_SIZE], sbox: SBox) -> [DNA; INPUT_SIZE] {
+    let mut result = [DNA::A; INPUT_SIZE];
+    result.copy_from_slice(&input.as_slice());
+    let (source, target) = result.split_at_mut(SOURCE_SIZE);
     let (intron_patterns, xor_selector) = key.split_at(KEY_SIZE - 2);
 
     let mut intron = [DNA::A; TARGET_SIZE];
@@ -62,7 +57,7 @@ fn round(
         // order is important - target must be the first argument
         target[i] = dna_xor(target[i], intron[i]);
     }
-    (source, target)
+    result
 }
 
 pub fn encrypt(input: Vec<DNA>, key: Vec<DNA>) -> Vec<DNA> {
@@ -78,15 +73,14 @@ pub fn encrypt(input: Vec<DNA>, key: Vec<DNA>) -> Vec<DNA> {
 
             while let Some(key_chunk) = key_chunks.next() {
                 // try_into changes slices to arrays of fixed length
-                let (h, t) = round(&input_chunk, key_chunk.try_into().unwrap(), sbox);
+                let result = round(&input_chunk, key_chunk.try_into().unwrap(), sbox);
                 if key_chunks.peek().is_some() {
                     // swap head with tail as per the Feistel algorithm
-                    input_chunk[0..TARGET_SIZE].copy_from_slice(&t);
-                    input_chunk[TARGET_SIZE..INPUT_SIZE].copy_from_slice(&h);
+                    input_chunk[0..TARGET_SIZE].copy_from_slice(&result[SOURCE_SIZE..INPUT_SIZE]);
+                    input_chunk[TARGET_SIZE..INPUT_SIZE].copy_from_slice(&result[0..SOURCE_SIZE]);
                 } else {
                     // for last round we need to keep the order to be able to decrypt the message
-                    input_chunk[0..SOURCE_SIZE].copy_from_slice(&h);
-                    input_chunk[SOURCE_SIZE..INPUT_SIZE].copy_from_slice(&t);
+                    input_chunk.copy_from_slice(&result);
                 }
             }
             input_chunk.to_vec()
@@ -114,19 +108,14 @@ pub fn decrypt(input: Vec<DNA>, key: Vec<DNA>) -> Result<Vec<DNA>, String> {
 
             while let Some(key_chunk) = key_chunks.next() {
                 // try_into changes slices to arrays of fixed length
-                let (mut h, mut t) = round(&input_chunk, key_chunk.try_into().unwrap(), sbox);
+                let result = round(&input_chunk, key_chunk.try_into().unwrap(), sbox);
                 if key_chunks.peek().is_some() {
                     // swap head with tail as per the Feistel algorithm
-                    input_chunk[0..SOURCE_SIZE].copy_from_slice(&h);
-                    input_chunk[SOURCE_SIZE..INPUT_SIZE].copy_from_slice(&t);
-                    t.copy_from_slice(&input_chunk[0..TARGET_SIZE]);
-                    h.copy_from_slice(&input_chunk[TARGET_SIZE..INPUT_SIZE]);
-                    input_chunk[0..SOURCE_SIZE].copy_from_slice(&h);
-                    input_chunk[SOURCE_SIZE..INPUT_SIZE].copy_from_slice(&t);
+                    input_chunk[0..SOURCE_SIZE].copy_from_slice(&result[TARGET_SIZE..INPUT_SIZE]);
+                    input_chunk[SOURCE_SIZE..INPUT_SIZE].copy_from_slice(&result[0..TARGET_SIZE]);
                 } else {
-                    // for last round we need to keep the order as it is
-                    input_chunk[0..SOURCE_SIZE].copy_from_slice(&h);
-                    input_chunk[SOURCE_SIZE..INPUT_SIZE].copy_from_slice(&t);
+                    // for last round we need to keep the order to be able to decrypt the message
+                    input_chunk.copy_from_slice(&result);
                 }
             }
             input_chunk.to_vec()
