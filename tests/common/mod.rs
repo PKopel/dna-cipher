c502a56519592ec2bad70e2a5b490863c2870b62
Paweł Kopel <pawel.kopel2@gmail.com>
2023-04-25 16:31:02 +0200

    move sac test back to tests

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,3 +1,5 @@
+// use std::collections::HashMap;
+
 use dnac::dna::{DNA, *};
 
 const INPUT_SIZE_BYTES: usize = 16;
@@ -68,3 +70,24 @@ pub fn check_ones(a: &u8) -> [u32; 8] {
     }
     result
 }
+
+// fn x2_test<K>(matrix: &[K], probabilities: HashMap<K, f64>) -> f64 {
+//     let mut bins = vec![0; probabilities.len()];
+//     for x in matrix.iter() {
+//         match x {
+//             _ if *x < 523857 => bins[0] += 1,
+//             _ if *x < 524158 => bins[1] += 1,
+//             _ if *x < 524417 => bins[2] += 1,
+//             _ if *x < 524718 => bins[3] += 1,
+//             _ => bins[4] += 1,
+//         }
+//     }
+//     let mut e = 0.0;
+//     bins.iter()
+//         .enumerate()
+//         .map(|(i, l)| {
+//             e = sac_probabilities(i);
+//             ((*l as f64) - e).powf(2.0) / e
+//         })
+//         .sum()
+// }
--- tests/sac_test.rs
+++ tests/sac_test.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::fs;
 
 use common::{check_ones, encrypt, xor_array};
 use kdam::tqdm;
@@ -20,28 +20,28 @@ fn sac_probabilities(x: usize) -> f64 {
 }
 
 fn x2_test(matrix: [[u32; 128]; 128]) -> f64 {
-    let mut bins = vec![vec![]; 5];
+    let mut bins = vec![0; 5];
     for x in matrix.iter().flat_map(|row| row.iter()) {
         match x {
-            _ if *x < 523857 => bins[0].push(x),
-            _ if *x < 524158 => bins[1].push(x),
-            _ if *x < 524417 => bins[2].push(x),
-            _ if *x < 524718 => bins[3].push(x),
-            _ => bins[4].push(x),
+            _ if *x < 523857 => bins[0] += 1,
+            _ if *x < 524158 => bins[1] += 1,
+            _ if *x < 524417 => bins[2] += 1,
+            _ if *x < 524718 => bins[3] += 1,
+            _ => bins[4] += 1,
         }
     }
     let mut e = 0.0;
     bins.iter()
-        .map(|v| v.len())
         .enumerate()
         .map(|(i, l)| {
             e = sac_probabilities(i);
-            ((l as f64) - e).powf(2.0) / e
+            ((*l as f64) - e).powf(2.0) / e
         })
         .sum()
 }
 
-fn main() -> io::Result<()> {
+#[test]
+fn sac_test() {
     let mut sac_matrix = [[0; 128]; 128];
     let data = include_bytes!("common/data/texts_16MB.blb");
     // let key = GenericArray::from([0u8; 16]);
@@ -68,8 +68,8 @@ fn main() -> io::Result<()> {
                 }
             });
     }
-    println!("{:?}", sac_matrix);
+    // println!("{:?}", sac_matrix);
+    fs::write("sac_matrix.txt", format!("{:?}", sac_matrix)).expect("Error saving SAC matrix");
     let val = x2_test(sac_matrix);
-    println!("{}", val);
-    Ok(())
+    assert!(val < 11.345)
 }
